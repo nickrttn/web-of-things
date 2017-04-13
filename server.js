844c07c66015bfd56c48221183ae31f2dcf2a2f0
Nick Rutten <hello@nickrttn.me>
2017-04-13 10:29:19 +0200

    barebones Express.js server

--- server.js
+++ server.js
@@ -1,2 +1,13 @@
 const express = require('express');
 
+const app = express();
+
+app.get('/', (req, res) => {
+	res.type('.html');
+	res.end('HALLO');
+});
+
+app.listen(process.env.PORT, (err) => {
+	if (err) throw err;
+	console.log(`----- Listening on http://localhost:${process.env.PORT} -----`);
+});
